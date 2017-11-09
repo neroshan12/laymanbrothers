9f151d775617a3383e3218c4488c14c10c7d9984
William <wmcabangon@gmail.com>
2017-11-09 14:13:24 +0000

    created controller for balance page with working model

--- src/main/java/domain/Account.java
+++ src/main/java/domain/Account.java
@@ -1,5 +1,49 @@
 package domain;
 
-public class Account {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
+public class Account {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String name;
+	private int balance;
+	
+	public Account() {	
+	}
+	
+	public Account(String name, int balance) {
+		super();
+		this.name = name;
+		this.balance = balance;
+	}
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
 }
