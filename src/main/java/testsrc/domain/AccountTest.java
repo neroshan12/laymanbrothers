563e989d863eb0175a74a76a980504382a5425f1
Joe Ingram <josephcingram@hotmail.co.uk>
2017-11-09 15:40:38 +0000

    Test suite for Account class created

---
+++ src/main/java/testsrc/domain/AccountTest.java
@@ -0,0 +1,49 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccountTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAccountStringInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBalance() {
+		fail("Not yet implemented");
+	}
+
+}
