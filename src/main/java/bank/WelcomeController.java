c78d80710fdadf4bd94f603630aa8c6f9754c6d1
Nero Siva <neroshan12@gmail.com>
2017-11-10 11:02:07 +0000

    Add new controller for homepage

---
+++ src/main/java/bank/WelcomeController.java
@@ -0,0 +1,16 @@
+package bank;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
+@Controller
+public class WelcomeController {
+	@RequestMapping("/homepage")
+	public String sayHi() {
+		return "homepage";
+	}
+}
