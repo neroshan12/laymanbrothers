c6a591bb220b4f52a11d9102d8b2535bc5af5526
Joe Ingram <josephcingram@hotmail.co.uk>
2017-11-12 16:52:53 +0000

    Bank class and test

---
+++ src/main/java/testsrc/domain/BankTest.java
@@ -0,0 +1,30 @@
+
+ package domain;
+ 
+ import static org.junit.Assert.*;
+ 
+ import org.junit.Before;
+ import org.junit.Test;
+ 
+ public class BankTest {
+ 	
+ private Bank classUnderTest;
+ 	
+	@Before
+ 	public void setUp() throws Exception {
+ 		classUnderTest = new Bank();
+ 	}
+ 
+ 	@Test
+     public void bank() {
+         assertTrue(classUnderTest instanceof Bank);
+     }
+ 
+     @Test
+    public void getUsers() {
+         User nero = new User("Nero", "Siva", "nero@nero.com", "nsiva");
+         classUnderTest.addUser(nero);
+         assertEquals("Nero", classUnderTest.getUsers());
+     }
+ 
+ }
