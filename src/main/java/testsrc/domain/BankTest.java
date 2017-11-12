3886a5605d1ef3c689ef484244981265acd34804
Joe Ingram <josephcingram@hotmail.co.uk>
2017-11-12 17:23:04 +0000

    Updateds to test to remove merge conflict lines

--- src/main/java/testsrc/domain/BankTest.java
+++ src/main/java/testsrc/domain/BankTest.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
  package domain;
  
@@ -20,8 +20,8 @@
      public void bank() {
          assertTrue(classUnderTest instanceof Bank);
      }
- 
-     @Test
+ 	
+    @Test
     public void getUsers() {
          User nero = new User("Nero", "Siva", "nero@nero.com", "nsiva");
          classUnderTest.addUser(nero);
@@ -29,34 +29,3 @@
      }
  
  }
-=======
-package domain;
-
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-public class BankTest {
-	
-private Bank classUnderTest;
-	
-	@Before
-	public void setUp() throws Exception {
-		classUnderTest = new Bank();
-	}
-
-	@Test
-    public void bank() {
-        assertTrue(classUnderTest instanceof Bank);
-    }
-
-    @Test
-    public void getUsers() {
-        User nero = new User("Nero", "Siva", "nero@nero.com", "nsiva");
-        classUnderTest.addUser(nero);
-        assertEquals("Nero", classUnderTest.getUsers());
-    }
-
-}
->>>>>>> d5cf1682e1f417561d4068eeac2e988673eff773
