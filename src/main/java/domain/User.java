a7b827fff3ac5594de150285e50bf11f5c8ee094
Nero Siva <neroshan12@gmail.com>
2017-11-13 16:30:10 +0000

    update branch from master

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -58,6 +58,24 @@ public class User {
 		currentAccount.balance += amount;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
 }
 
 
