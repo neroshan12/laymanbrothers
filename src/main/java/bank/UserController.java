3b1e1e0fe1bc9cbb12cc4c4f586ee8994f53714e
Nero Siva <neroshan12@gmail.com>
2017-11-13 15:10:50 +0000

    Add sign up functionality

--- src/main/java/bank/UserController.java
+++ src/main/java/bank/UserController.java
@@ -1,10 +1,16 @@
 package bank;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import domain.User;
 
@@ -12,15 +18,23 @@ import domain.User;
 @Controller
 public class UserController {
 	
-	@GetMapping(value = "/newUser")
-	public String userForm(Model model) {
-		model.addAttribute("user", new User());
+	ArrayList<User> someList = new ArrayList<User>();
+//	User user = new User("Will", "Cabangon", "will@will.com", "will_will");
+	
+	@RequestMapping(value = "/newUser")
+	public String userForm() {
 		return "newUser";
 	}
-	@PostMapping("/newUser")
-    public String newUserSubmit(@ModelAttribute User user) {
-        return "user";
+	
+	@RequestMapping(value = "/newUser", method = RequestMethod.POST)
+    public String newUserSubmit(@ModelAttribute("user") User user, Model model) {
+		someList.add(user);
+        return "redirect:/user";
     }
 	
-	
+	@RequestMapping(value = "/user")
+	public String user(Model model) {
+	model.addAttribute("user", someList.get(0));
+		return "user";
+	}
 }
--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -63,6 +63,24 @@ package domain;
 	
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
 }
 
 
