923bb268331e6bfd1545a0ae8f89222d53814795
keith(mac) <ka33145@googlemail.com>
2017-11-14 14:06:11 +0000

    Updated getAllusers. Added findById, findByName,transfer method to Bank

--- src/main/java/domain/Bank.java
+++ src/main/java/domain/Bank.java
@@ -16,8 +16,37 @@ import java.util.List;
          users.add(user);
      }
      
- 	public User getUsers() {
- 		return users.get(0);
+     public List<User> getAllUsers() {
+  		return users;
+  	}
+  	
+  	public User findById(int id)
+  	{
+  		return users.get(id);
+  	}
+  	
+  	public User findByName(String username)
+ 	{
+ 		for(int i=0; i < users.size(); i++)
+ 		{
+ 			System.out.println(users.get(i).getUsername());
+ 			if(users.get(i).getUsername() == username)
+ 			{
+ 				return users.get(i);
+ 			}
+ 		}
+ 		return null;
  	}
- 	
+  	
+  	public void transfer(double amount, String fromUserName,String toUserName)
+  	{
+  	  System.out.println(this.findByName(fromUserName));
+  	  User fromUser = this.findByName(fromUserName);
+  	  User toUser = this.findByName(toUserName);
+  	  System.out.println(toUser.currentAccount.getBalance());
+  	  System.out.println(fromUser.currentAccount.getBalance());
+  	  fromUser.getCurrentAccount().transfer(amount, toUser.currentAccount);
+  	  System.out.println(toUser.currentAccount.getBalance());
+  	  System.out.println(fromUser.currentAccount.getBalance());
+  	}
  }
